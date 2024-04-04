42131d32c25e4284f52a66fae89df7dc3c79da6c
Disha Gupta <148881701+Dishagupta05@users.noreply.github.com>
2024-04-04 21:25:07 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,128 @@
+// console.log('hello world');
+
+// var message='my name';
+// console.log(message);
+
+// var age=18;
+// age="done";
+
+// function env(){
+//     console.log("else");
+// }
+// const message='hello';
+// console.log(message);
+
+
+// let age='20';
+// console.log(age);
+
+// let userAge='abcd';
+// userAge='abcd';
+// console.log(_);
+
+// let arr=['a','b','c','d'];
+// arr[0]='abcd';
+// console.log(arr);
+
+
+// let str='12';
+// let age='20';
+// let ans=age+str;
+// console.log(ans);
+
+
+// let str=12;
+// let age=20;
+// let ans=age+str;
+// console.log(ans);
+
+// let str='12';
+// const p=parseint(str);
+// let n=Number(str);
+// console.log(p,n);
+
+// const str='khan';
+// const userName='My Name is:'
+// const ans=`${userName}${str}`;
+// console.log(ans);
+
+// function print(str){
+//     console.log(str);
+// }
+
+// const s='Hello';
+// print(s);
+
+// print();
+
+// function sum(a,b){
+//     if (a && b){
+//     console.log(a+b);
+//     }
+//     else
+//     console.log('no value')
+// }
+// sum();
+
+
+//0 , null, undefined, NaN,'',false
+
+
+//function declaration
+
+// function print(){
+//     console.log('--');
+
+// }
+
+// //function assignment
+
+// const a=function print(){
+//     console.log('--');
+// }
+
+// //function assignment (annonymous)
+
+// const b=function(){
+//     console.log('--');
+    
+// }
+
+// let c=()=>{
+//     console.log("--");
+// }
+
+// print();
+
+// a();
+// b();
+// c();
+
+
+
+// const obj=new Object();
+// obj[12]='twelve';
+// obj[1]='one';
+// obj[2]='two';
+// obj['name']='disha';
+// console.log(obj);
+
+
+// const obj={
+//     1:'one',
+//     'name':'disha',
+
+// }
+// console.log(obj);
+
+const obj1={
+    'first name':'disha',
+
+}
+
+const x=prompt('please tell me' );
+
+console.log(x);
+
+
+
